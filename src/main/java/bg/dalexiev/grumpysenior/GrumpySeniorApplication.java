aea77c3ccb451c5c8624a4c82e03fd4dc889aa62
Danail Alexiev <--local>
2026-09-02 23:34:22 +0300

    Clean up uneeded code

--- src/main/java/bg/dalexiev/grumpysenior/GrumpySeniorApplication.java
+++ src/main/java/bg/dalexiev/grumpysenior/GrumpySeniorApplication.java
@@ -1,21 +1,15 @@
 package bg.dalexiev.grumpysenior;
 
 import jakarta.annotation.Resource;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class GrumpySeniorApplication implements CommandLineRunner  {
+public class GrumpySeniorApplication {
 
-    @Resource
-    PasswordEncoder passwordEncoder;
-
-    public static void main(String[] args) { SpringApplication.run(GrumpySeniorApplication.class, args); }
-
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("Encrypted admin password: " + passwordEncoder.encode("secret123"));
+    static void main(String[] args) {
+        SpringApplication.run(GrumpySeniorApplication.class, args);
     }
+
 }
